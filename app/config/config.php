3f6741acd715f453eede4d0da942e9ab89a9ca91
TADAS-PC\taaas <tadas.mockvicius@gmail.com>
2018-08-19 20:04:51 +0300

    MVC core su Post model

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,9 @@
 <?php
-/**
- *
- */
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc_db');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,12 +1,19 @@
 <?php
 class Pages extends Controller {
     public function __construct(){
+        $this->postModel = $this->model('Post');
 
     }
 
     public function index(){
-        $data = ['title' => 'Welcome'];
+        $posts = $this->postModel->getPosts();
+
+        $data = [
+            'title' => 'Welcome',
+            'posts' => $posts
+                ];
         $this->view('pages/index', $data);
+
     }
 
     public function about(){
